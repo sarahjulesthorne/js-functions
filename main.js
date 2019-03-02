ee588fb6bd8ad00b998652275a310b2bf1e9c551
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-03-02 11:02:07 -0600

    adding modified JS

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log('Hey there');
+const firstName = 'Sarah';
+const lastName = 'Thorne';
+const firstName1 = 'Lillah';
+const lastName1 = 'Watson';
+//console.log(firstName + ' ' + lastName);
+console.log(`${firstName} ${lastName}`);
+console.log(`${firstName1} ${lastName1}`);
+const namePrinter = (firstName, lastName) => {
+    console.log(`${firstName} ${lastName}`);
+};
+namePrinter('Sarah', 'Thorne');
+namePrinter('Lillah', 'Watson');
+namePrinter('Nice', 'Person');
+namePrinter('Exactly', 'Right');
+namePrinter('Interesting', 'Idea');
